f98eecc5e554b71764372c090f4fbefa93faec77
egor <egorbiceps228@mail.ru>
2022-08-12 15:02:29 +0300

    minus garbage 2.0

--- TottoriApp/TabBar/TabBarViewController.swift
+++ TottoriApp/TabBar/TabBarViewController.swift
@@ -8,28 +8,11 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-//    lazy var countView : BadgeView = {
-//        var countView  = BadgeView()
-//        countView.translatesAutoresizingMaskIntoConstraints = false
-//
-//
-//
-//
-//        return countView
-//
-//
-//    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tabBar.addSubview(countView)
-//        countView.bottomAnchor.constraint(equalTo: tabBar.topAnchor,constant: 39).isActive = true
-//        countView.centerXAnchor.constraint(equalTo: tabBar.centerXAnchor,constant: (UIScreen.main.bounds.width / 3) + 14).isActive = true
-//        countView.widthAnchor.constraint(equalToConstant: 20).isActive = true
-//        countView.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
-        
-        tabBar.itemSpacing = UIScreen.main.bounds.width / 3
         tabBar.tintColor = .red
         
         var image1 = UIImage(named: "union")?.withTintColor(.black,renderingMode: .alwaysOriginal)
@@ -45,23 +28,18 @@ class TabBarViewController: UITabBarController {
             image3 = UIImage.resizeImage(image: unImage3, targetSize: CGSize(width: 34, height: 34))
         }
         
-
-        
-    
         let catalogVC = UINavigationController(rootViewController: CatalogViewController())
         let catalogVCTabBar = UITabBarItem(title: "",image: image1, tag: 1)
         catalogVC.tabBarItem = catalogVCTabBar
-        
         
         let aboutUsVC =  AboutUsViewController()
         let aboutUsTabBar = UITabBarItem(title: "",image: image2, tag: 2)
         aboutUsVC.tabBarItem = aboutUsTabBar
         
-        
         let basketVC = UINavigationController(rootViewController: BasketViewController())
         let basketVCTabBar = UITabBarItem(title: "", image: image3, tag: 3)
         basketVC.tabBarItem = basketVCTabBar
-        
+
         setViewControllers([catalogVC,aboutUsVC,basketVC], animated: true)
 
         // Do any additional setup after loading the view.
