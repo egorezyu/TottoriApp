d87aabedc2411bbd3397bd55ff6a451b6f90275a
egor <egorbiceps228@mail.ru>
2022-09-12 20:18:22 +0300

    book carousel last commit)

--- TottoriApp/SharedAnimation/BookTransitionManager.swift
+++ TottoriApp/SharedAnimation/BookTransitionManager.swift
@@ -38,11 +38,11 @@ final class BookTransitionManager: NSObject, UIViewControllerAnimatedTransitioni
         var transform1 = CATransform3DIdentity
         transform1.m34 = -1 / 500
         transform1 = CATransform3DRotate(transform1, -.pi / 4, 0, 1, 0)
-        transform1 = CATransform3DTranslate(transform1, -200, 0, 0)
+        transform1 = CATransform3DTranslate(transform1, -300, 0, 0)
         var transform2 = CATransform3DIdentity
         transform2.m34 = -1 / 500
 //        transform2 = CATransform3DRotate(transform2, -.pi / 6, 0, 1, 0)
-        transform2 = CATransform3DTranslate(transform2, -200, 0, 0)
+        transform2 = CATransform3DTranslate(transform2, -300, 0, 0)
         var transform3 = CATransform3DIdentity
         transform3 = CATransform3DTranslate(transform3, -UIScreen.main.bounds.width, 0, 0)
         var transform4 = CATransform3DIdentity
@@ -59,14 +59,14 @@ final class BookTransitionManager: NSObject, UIViewControllerAnimatedTransitioni
 
         UIView.animateKeyframes(withDuration: duration, delay: 0, animations: {
 
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.8) {
                 fromViewController.view.layer.transform = transform1
                 toView.layer.transform = transform2
                 
       
           }
 
-          UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+          UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.2) {
               toView.layer.transform = transform3
               fromViewController.view.layer.transform = transform3
 //
